89b20dff60c9b0274d204ca405893a83c36dc51d
Sachin GB <gumblapu@usc.edu>
2018-03-01 00:06:07 -0800

    Updating featrization and similarity code

--- py_build_features.py
+++ py_build_features.py
@@ -2,7 +2,6 @@
 # Will need to write more on this
 
 import pandas as pd
-from geopy.distance import vincenty
 from collections import defaultdict
 
 # Four categories defined, can add more
@@ -11,11 +10,12 @@ SMALL = 1
 MEDIUM = 2
 LARGE = 3
 
-NA = "Not Available"
+NA = "NA"
+BLANK = ""
 
 # Input file goes here
-DATA_SET_PATH = '../Additional_Data/meteor_data_appeneded.csv'
-OUTPUT_DATA_FRAME_NAME = '../Additional_Data/Latest_Merged_Data_0228714pm.csv'
+DATA_SET_PATH = 'ufo_dataset_final.csv'
+OUTPUT_DATA_FRAME_NAME = 'featurized_data_set.csv'
 
 # TODO: Need to add encoding when reading the data frame
 data = pd.read_csv(DATA_SET_PATH)
@@ -25,12 +25,12 @@ data = pd.read_csv(DATA_SET_PATH)
 SMALL_AD_TRESHOLD = [[0,50], [50,100], [100]]
 MEDIUM_AD_TRESHOLD = [[0,50], [50,100], [100]]
 LARGE_AD_TRESHOLD = [[0,50], [50,100], [100]]
-METORITE_TRESHOLD = [[1,100], [100,300], [300]]
+METRO_DISTANCE_TRESHOLD = [[0,50], [50,100], [100]]
 
 small_airport_dist_category = []
 medium_airport_dist_category = []
 large_airport_dist_category = []
-metorite_dist_category = []
+metro_distance_category = []
 
 # utility function which takes the reference and gives the category
 def get_treshold_value(reference_tresh, val):
@@ -48,24 +48,24 @@ for index, row in data.iterrows():
     small_ad = row['closest_SMALL_airport_distance']
     medium_ad = row['closest_MEDIUM_airport_distance']
     large_ad = row['closest_LARGE_airport_distance']
-    meteor_dist = row['distance_to_meteor']
+    metro_distance = row['closest_metro_distance']
 
     small_airport_dist_category.append(get_treshold_value(SMALL_AD_TRESHOLD, small_ad))
     medium_airport_dist_category.append(get_treshold_value(MEDIUM_AD_TRESHOLD, medium_ad))
     large_airport_dist_category.append(get_treshold_value(LARGE_AD_TRESHOLD, large_ad))
-    metorite_dist_category.append(get_treshold_value(METORITE_TRESHOLD, meteor_dist))
+    metro_distance_category.append(get_treshold_value(METRO_DISTANCE_TRESHOLD, metro_distance))
 
 
 # Just making sure that all have the same shape
-print len(metorite_dist_category)
 print len(large_airport_dist_category)
 print len(medium_airport_dist_category)
 print len(small_airport_dist_category)
+print len(metro_distance_category)
 
-data['metorite_category'] = metorite_dist_category
 data['large_airport_category'] = large_airport_dist_category
 data['medium_airport_category'] = medium_airport_dist_category
 data['small_airport_category'] = small_airport_dist_category
+data['metro_distance_category'] = metro_distance_category
 
 # TODO: Add encoding before exporting as csv
 data.to_csv(OUTPUT_DATA_FRAME_NAME, index=False)
--- py_hw1_similarity.py
+++ py_hw1_similarity.py
@@ -21,7 +21,7 @@ class Vector:
     # Added this function to generate a string from the feature vector
     def stringify(self,feature):
         attribute_value = feature.values()
-        print attribute_value
+        # print attribute_value
         attribute_value = [str(x) for x in attribute_value]
         if isinstance(attribute_value, list):
             return str((", ".join(attribute_value)).encode('utf-8').strip())
@@ -57,21 +57,23 @@ class Vector:
 
 
 # Input / Output Files go here
-INPUT_DATA_FILE = "../Additional_Data/Latest_Merged_Data_0228714pm.csv"
-OUTPUT_CSV_FILE = "../cosine_sim_airport.csv"
+INPUT_DATA_FILE = "featurized_data_set.csv"
+EDIT_DIST_CSV_FILE = "edit_distance_similarity.csv"
+COSINE_SIMILARITY_CSV_FILE = "cosine_similarity.csv"
 
 
 
 # ######################## Sampling the input data and normalizing the columns. To be improved and investigated further
 data = pd.read_csv(INPUT_DATA_FILE)
 
-data = data.sample(10)
+data = data.loc[data['state'] == 'California']
+
+# data = data.sample(10)
 data_dictionary = defaultdict()
 
 # ########################################################################################################
 
 # print data['closest_SMALL_airport_distance']
-
 
 # Create a dictionary of items - Key: Location of the ufo sighting, value- feature
 for index,row in data.iterrows():
@@ -80,22 +82,48 @@ for index,row in data.iterrows():
     feature['medium_airport'] = row['medium_airport_category']
     feature['small_airport'] = row['small_airport_category']
     feature['large_airport'] = row['large_airport_category']
-    feature['meteorite'] = row['metorite_category']
-    data_dictionary[row['location']] = feature
-
+    feature['meteorite'] = row['meteor_sighting']
+    feature['metro_distance'] = row['metro_distance_category']
+    feature['closest_metro_m4'] = row['closest_metro_m4']
+    feature['closest_metro_m6'] = row['closest_metro_m6']
+    # feature['population'] = row['population']
+    location = row['location'].split(',')[0]
+    data_dictionary[row['id']] = feature
 
 tuples = itertools.combinations(data_dictionary.keys(),2)
-for sighting_1, sighting_2 in tuples:
-    # try:
-    raw_edit_distance = [sighting_1, sighting_2]
-    v1 = Vector(sighting_1, data_dictionary[sighting_1])
-    v2 = Vector(sighting_2, data_dictionary[sighting_2])
-    print sighting_1, sighting_2 , " = ", v1.editDistance(v2)
-        # raw_edit_distance.append(round(v1.editDistance(v2),2))
-        # print raw_edit_distance
-    # except:
-    #     continue
 
+
+def compute_cosine_similarity(tuples):
+    with open(COSINE_SIMILARITY_CSV_FILE, "wb") as outF:
+        a = csv.writer(outF, delimiter=',')
+        a.writerow(["x-coordinate","y-coordinate","Similarity_score"])
+        for sighting_1, sighting_2 in tuples:
+            try:
+                raw_cosine_distance = [sighting_1, sighting_2]
+                v1 = Vector(sighting_1, data_dictionary[sighting_1])
+                v2 = Vector(sighting_2, data_dictionary[sighting_2])
+                raw_cosine_distance.append(round(v1.cosTheta(v2),2))
+                a.writerow(raw_cosine_distance)
+            except:
+                continue
+
+def compute_edit_distance(tuples):
+    with open(EDIT_DIST_CSV_FILE, "wb") as outF:
+        a = csv.writer(outF, delimiter=',')
+        a.writerow(["x-coordinate","y-coordinate","Similarity_score"])
+        for sighting_1, sighting_2 in tuples:
+            try:
+                raw_edit_distance = [sighting_1, sighting_2]
+                v1 = Vector(sighting_1, data_dictionary[sighting_1])
+                v2 = Vector(sighting_2, data_dictionary[sighting_2])
+                raw_edit_distance.append(round(v1.editDistance(v2),2))
+                a.writerow(raw_edit_distance)
+            except:
+                continue
+
+
+# compute_edit_distance(tuples)
+compute_edit_distance(tuples)
 
 # create tuples for ufo sighting and compute their cosine similarity and write them to a file.
 # with open(OUTPUT_CSV_FILE, "wb") as outF:
