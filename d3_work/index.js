d03ba75f18cbd5db72276a44671dc80b12abe9ee
Sachin GB <gumblapu@usc.edu>
2018-03-02 05:54:08 -0800

    Adding missing files in D3

--- d3_work/index.js
+++ d3_work/index.js
@@ -57,12 +57,13 @@ d3.json('usa.json', function(error, usa){
 					meteor_coords = []
 					major_metro_coords = []
 					airport_coords = []
+					satellite_coords = [[-80.52662,28.45077], [-120.5155986,34.7425933], [-75.45752345,37.93843385], [-74.151876, 40.012573], [-152.342484, 57.4346198]]
 					aa = [-118.24368, 34.05224];
 					bb = [-118.24368, 34.05224];
 
 					var color_hash = {  
 						0 : ["UFO Sighting", "green"],
-						1 : ["Airport Location", "yellow"]
+						1 : ["Satellite Launch Station", "red"]
 						// 2 : ["Major Sport Sighting", "yellow"]
 					}
 
@@ -77,8 +78,9 @@ d3.json('usa.json', function(error, usa){
 						try {
 							var rowID = filteredData[i]['id']
 							if (id_added.indexOf(rowID) == -1){
+								console.log(filteredData[i]['metorite_long']);
 								ufo_coords.push([filteredData[i]["geocoded_longitude"], filteredData[i]["geocoded_latitude"]])
-								meteor_coords.push([filteredData[i]['meteorite_lon'], filteredData[i]['meteorite_lat']])
+								meteor_coords.push([filteredData[i]['metorite_long'], filteredData[i]['metorite_lat']])
 								major_metro_coords.push([filteredData[i]['sports_metro_lon'], filteredData[i]['sports_metro_lat']])
 								airport_coords.push([filteredData[i]['airport_lon'], filteredData[i]['airport_lat']])
 								id_added.push(rowID)
@@ -90,41 +92,41 @@ d3.json('usa.json', function(error, usa){
 						}
 					}
 
-					console.log(data2)
+					// console.log(data2)
 
-					for(var j=0; j<data2.length; j++){
-						// console.log(data2[j]['coordinates'].replace(/\s/g,'').split(','))
-						coords = data2[j]['coordinates'].replace(/\s/g,'').split(',')
-						// console.log(coords[0])
-						// break;
-						airport_coords.push([coords[0], coords[1]])
-					}
+					// for(var j=0; j<data2.length; j++){
+					// 	// console.log(data2[j]['coordinates'].replace(/\s/g,'').split(','))
+					// 	coords = data2[j]['coordinates'].replace(/\s/g,'').split(',')
+					// 	// console.log(coords[0])
+					// 	// break;
+					// 	airport_coords.push([coords[0], coords[1]])
+					// }
 
-					console.log(id_added.length)
+
 
 					dataset[0] = ufo_coords
-					dataset[1] = airport_coords
+					dataset[1] = satellite_coords
 					// dataset[2] = sports_coords
 
 
 
 					svg.selectAll("rect")
-						.data(dataset[1]).enter()
+						.data(dataset[0]).enter()
 						.append("rect")
 						.attr("x", function(d){ return projection(d)[0]; })
 						.attr("y", function(d){ return projection(d)[1]; })
-						.attr("width", "7px")
-						.attr("height", "7px")
-						.attr("fill", "yellow")
+						.attr("width", "4px")
+						.attr("height", "4px")
+						.attr("fill", "green")
 
 
 					svg.selectAll("circle")
-						.data(dataset[0]).enter()
+						.data(dataset[1]).enter()
 						.append("circle")
 						.attr("cx", function(d){ return projection(d)[0]; })
 						.attr("cy", function(d){ return projection(d)[1]; })
-						.attr("r", "2px")
-						.attr("fill", "green")
+						.attr("r", "10px")
+						.attr("fill", "red")
 						.style("opacity", 1)
 
 
